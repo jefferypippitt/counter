8c4482e7199f413112995da6c3e92db8eba775a3
Jeffery <jefferyspippitt@gmail.com>
2024-03-23 20:19:05 -0500

    theme and header finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { ThemeToggle } from "@/components/ui/theme-toggle";
+
 export default function Home() {
   return (
-    <section className='py-24'>
-      <div className='container'>
-        <h1 className='text-3xl font-bold'>Next TS Starter</h1>
-      </div>
-    </section>
+    <div className='text-3xl font-bold mx-auto max-w-2xl p-6 flex items-center justify-between border-b-2 pb-2'>
+      <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0"></h2>
+        Counter App
+      <ThemeToggle />
+    </div>
   )
 }
